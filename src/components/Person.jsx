4b4aafee5ad5f59f92cf8d6d63d3f6bd65dfc04d
vaishna1vi <vaishnavinagvanshi68675@gmail.com>
2023-04-17 23:52:36 +0530

    learned UseState and props

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 
-export default function Person({name, reg_no, city, age, 
-}) {
+// export default function Person({name, reg_no, city, age, 
+// }) {
 
-  
+export default function Person(props) {
     
   return (
     <div>
-        {name} {reg_no} {city} {age}
+        {props.name} {props.reg_no} {props.city} {props.age}
+        {/* {name} {reg_no} {city} {age} */}
+
       {" "}  
 
     </div>
   )
+
+}
+
+Person.defaultProps = {
+  name : 'NA',
+  age : 'NA',
 }
